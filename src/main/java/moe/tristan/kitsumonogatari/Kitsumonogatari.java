90c762f91d49c8d9eaf34aaef694857ae244ecbf
Tristan971 <tristandeloche@gmail.com>
2018-08-27 01:09:59 +0100

    Initial commit

---
+++ src/main/java/moe/tristan/kitsumonogatari/Kitsumonogatari.java
@@ -0,0 +1,13 @@
+package moe.tristan.kitsumonogatari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kitsumonogatari {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kitsumonogatari.class, args);
+    }
+
+}
