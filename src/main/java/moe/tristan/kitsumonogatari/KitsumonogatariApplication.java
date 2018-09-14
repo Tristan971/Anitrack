764178084cb3d5fe59e06dd2c4b2e938fe856843
Tristan Deloche <tristandeloche@gmail.com>
2018-09-14 14:27:47 +0100

    Move back to Java from Kotlin

---
+++ src/main/java/moe/tristan/kitsumonogatari/KitsumonogatariApplication.java
@@ -0,0 +1,12 @@
+package moe.tristan.kitsumonogatari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitsumonogatariApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KitsumonogatariApplication.class, args);
+    }
+}
